2f88049b0060d29d328e836f95d684a52befdfc9
Jesse Viljoen <jesse.viljoen@capaciti.org.za>
2025-04-16 13:30:18 +0200

    Update face_main_api.js

--- JavaScript/face_main_api.js
+++ JavaScript/face_main_api.js
@@ -1,50 +1,5 @@
 :      https://raw.githubusercontent.com/karkranikhil/face-recognition-using-js/master/js/face-api.min.js
-const video = document.getElementById("video");
-const isScreenSmall = window.matchMedia("(max-width: 700px)");
-/****Loading the model ****/
-Promise.all([  
-faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  ]).then(startVideo);
-function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => (video.srcObject = stream),
-    err => console.error(err)
-  );
-}
-/****Fixing the video with based on size size  ****/
-function screenResize(isScreenSmall) {
-  if (isScreenSmall.matches) {
-    video.style.width = "320px";
-  } else {
-    video.style.width = "500px";
-  }
-}
-const video = document.getElementById("video");
-const isScreenSmall = window.matchMedia("(max-width: 700px)");
-/****Loading the model ****/
-Promise.all([  
-faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  ]).then(startVideo);
-function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => (video.srcObject = stream),
-    err => console.error(err)
-  );
-}
-/****Fixing the video with based on size size  ****/
-function screenResize(isScreenSmall) {
-  if (isScreenSmall.matches) {
-    video.style.width = "320px";
-  } else {
-    video.style.width = "500px";
-  }
-}
+
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -69,4 +24,50 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+const video = document.getElementById("video");
+const isScreenSmall = window.matchMedia("(max-width: 700px)");
+/****Loading the model ****/
+Promise.all([  
+faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  ]).then(startVideo);
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => (video.srcObject = stream),
+    err => console.error(err)
+  );
+}
+/****Fixing the video with based on size size  ****/
+function screenResize(isScreenSmall) {
+  if (isScreenSmall.matches) {
+    video.style.width = "320px";
+  } else {
+    video.style.width = "500px";
+  }
+}
+const video = document.getElementById("video");
+const isScreenSmall = window.matchMedia("(max-width: 700px)");
+/****Loading the model ****/
+Promise.all([  
+faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  ]).then(startVideo);
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => (video.srcObject = stream),
+    err => console.error(err)
+  );
+}
+/****Fixing the video with based on size size  ****/
+function screenResize(isScreenSmall) {
+  if (isScreenSmall.matches) {
+    video.style.width = "320px";
+  } else {
+    video.style.width = "500px";
+  }
+}
 
