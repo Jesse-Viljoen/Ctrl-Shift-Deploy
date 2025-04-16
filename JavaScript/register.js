4765c2fe55183b321bab02e37801564f2bfff254
Jesse Viljoen <jesse.viljoen@capaciti.org.za>
2025-04-16 14:55:51 +0200

    Update register.js

--- JavaScript/register.js
+++ JavaScript/register.js
@@ -25,6 +25,15 @@ const storage = firebase.storage();
 const database = firebase.database();
 
 
+// Hashing function (SHA-256) to compare passwords
+        async function hashPassword(password) {
+            const encoder = new TextEncoder();
+            const data = encoder.encode(password);
+            const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+            const hashArray = Array.from(new Uint8Array(hashBuffer));
+            const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+            return hashHex;
+        }
 
 // Sign-up function
 async function signUpUser(full_name, email,phone, password) {
