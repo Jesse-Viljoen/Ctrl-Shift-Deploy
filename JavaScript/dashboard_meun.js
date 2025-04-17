1c6215d3eb0c87c8abac86496a66f346ee4c5105
Jesse Viljoen <jesse.viljoen@capaciti.org.za>
2025-04-17 08:39:23 +0200

    Update dashboard_meun.js

--- JavaScript/dashboard_meun.js
+++ JavaScript/dashboard_meun.js
@@ -1,1 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDoDiJ9-UzKfuwBLS3f4N-4V96vgE2hNEY",
+  authDomain: "ctrl-shift-deploy.firebaseapp.com",
+  databaseURL: "https://ctrl-shift-deploy-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ctrl-shift-deploy",
+  storageBucket: "ctrl-shift-deploy.firebasestorage.app",
+  messagingSenderId: "1008311150868",
+  appId: "1:1008311150868:web:5d8db4655fbe8de360ba01",
+  measurementId: "G-HXZWM4BW31"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
