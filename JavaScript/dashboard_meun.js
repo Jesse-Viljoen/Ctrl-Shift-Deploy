3e12a6703ad228cba15582742b83f34c6d250e8d
Tashreeq Engelbrecht <tashreeqengelbrecht69@gmail.com>
2025-04-22 13:37:33 +0200

    updated code

--- JavaScript/dashboard_meun.js
+++ JavaScript/dashboard_meun.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
+// Import Firebase modules
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDoDiJ9-UzKfuwBLS3f4N-4V96vgE2hNEY",
   authDomain: "ctrl-shift-deploy.firebaseapp.com",
   databaseURL: "https://ctrl-shift-deploy-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "ctrl-shift-deploy",
-  storageBucket: "ctrl-shift-deploy.firebasestorage.app",
+  storageBucket: "ctrl-shift-deploy.appspot.com", // ✅ Fixed a typo here
   messagingSenderId: "1008311150868",
   appId: "1:1008311150868:web:5d8db4655fbe8de360ba01",
   measurementId: "G-HXZWM4BW31"
